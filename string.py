9ebd17523232aee8af7e5f4cad5a71b6b5e45770
Analyst Pramod <105561410+pramodryo@users.noreply.github.com>
2022-07-02 17:00:48 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,15 @@
+#  write a latter Format
+
+letter = '''Dear <|NAME|>,
+Greetings from ABC coding house. I am happy to tell you about your selection
+You are selected!
+Have a great day ahead!
+Thanks and regards,
+Bill
+Date: <|DATE|>
+'''
+name = input("Enter Your Name\n")
+date = input("Enter Date\n")
+letter = letter.replace("<|NAME|>", name)
+letter = letter.replace("<|DATE|>", date)
+print(letter)
